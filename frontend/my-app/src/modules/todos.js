cb3d31d7037b6be858c00c0ab3789b7320d391af
gunkim0318 <45007556+gunkim0318@users.noreply.github.com>
2020-08-05 02:40:02 +0900

    리덕스 리듀서 작성

--- frontend/my-app/src/modules/todos.js
+++ frontend/my-app/src/modules/todos.js
@@ -1,42 +1,86 @@
-const SET_REVERSE_CHECK_TODO = "SET_REVERSE_CHECK_TODO";
-const CREATE_TODO = "CREATE_TODO";
+import axios from "axios";
+
+// const CREATE_TODO = "CREATE_TODO";
+// const CREATE_TODO_SUCCESS = "CREATE_TODO_SUCCESS";
+// const CREATE_TODO_ERROR = "CREATE_TODO_ERROR";
+
+const GET_TODOS = "GET_TODOS";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_ERROR = "GET_TODOS_ERROR";
+
 const DELETE_TODO = "DELETE_TODO";
+const DELETE_TODO_SUCCESS = "DELETE_TODO_SUCCESS";
+const DELETE_TODO_ERROR = "DELETE_TODO_ERROR";
 
-let nextId = 5;
+export const deleteTodo = (id) => async (dispatch) => {
+  dispatch({ type: DELETE_TODO });
 
-const initialState = [
-  {
-    id: 1,
-    check: true,
-    text: "리액트 공부하기",
-  },
-  {
-    id: 2,
-    check: false,
-    text: "리액트 안녕",
-  },
-  {
-    id: 3,
-    check: true,
-    text: "리액트 수고",
-  },
-  {
-    id: 4,
-    check: false,
-    text: "리액트 잘 있어",
-  },
-];
+  try {
+    await axios.delete(`/api/todo/${id}`);
 
-export default function todos(state = initialState, action) {
+    const response = await axios.get("/api/todo/list");
+    dispatch({ type: DELETE_TODO_SUCCESS, data: response.data });
+  } catch (e) {
+    dispatch({ type: DELETE_TODO_ERROR });
+  }
+};
+
+export const getTodos = () => async (dispatch) => {
+  dispatch({ type: GET_TODOS });
+
+  try {
+    const response = await axios.get("/api/todo/list");
+    dispatch({ type: GET_TODOS_SUCCESS, data: response.data });
+  } catch (e) {
+    dispatch({ type: GET_TODOS_ERROR });
+  }
+};
+
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
+export default function todos_test(state = initialState, action) {
   switch (action.type) {
-    case SET_REVERSE_CHECK_TODO:
-      return state.map((todo) =>
-        todo.id === action.id ? { ...todo, check: !todo.check } : todo
-      );
-    case CREATE_TODO:
-      return [...state, { id: nextId++, check: false, text: action.text }];
+    case GET_TODOS:
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    case GET_TODOS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.data,
+        error: false,
+      };
+    case GET_TODOS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
     case DELETE_TODO:
-      return state.filter((todo) => todo.id !== action.id);
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    case DELETE_TODO_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.data,
+        error: false,
+      };
+    case DELETE_TODO_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
     default:
       return state;
   }
