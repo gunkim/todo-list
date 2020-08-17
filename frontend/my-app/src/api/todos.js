973cbf8f3fc9bf2329703cb133c0aadb9084c078
gunkim0318 <gunkim0318@gmail.com>
2020-08-17 14:39:37 +0900

    필요없는 로깅 삭제

--- frontend/my-app/src/api/todos.js
+++ frontend/my-app/src/api/todos.js
@@ -3,21 +3,18 @@ import axios from "axios";
 const BASE_API_URI = "/api/todo";
 
 export const deleteTodo = async (jwtToken, id) => {
-  console.log("delete token : " + jwtToken);
   const response = await axios.delete(`${BASE_API_URI}/${id}`, {
     headers: { Authorization: jwtToken },
   });
   return response.status;
 };
 export const getTodos = async (jwtToken) => {
-  console.log("get token : " + jwtToken);
   const response = await axios.get(`${BASE_API_URI}/list`, {
     headers: { Authorization: jwtToken },
   });
   return response.data;
 };
 export const createTodo = async (jwtToken, text) => {
-  console.log("create token : " + jwtToken);
   const response = await axios.post(
     BASE_API_URI,
     { text: text },
@@ -28,7 +25,6 @@ export const createTodo = async (jwtToken, text) => {
   return response.status;
 };
 export const setReverseCheckTodo = async (jwtToken, id) => {
-  console.log("reverse token : " + jwtToken);
   const response = await axios.put(`${BASE_API_URI}/${id}`, null, {
     headers: { Authorization: jwtToken },
   });
