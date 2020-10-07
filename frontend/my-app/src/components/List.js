ba181defedb08fe28c87c766640341c703ac97ef
gunkim <gunkim0318@gmail.com>
2020-10-07 23:24:38 +0900

    입력된 할 일이 없을 경우 알림 메시지가 뜨도록 수정

--- frontend/my-app/src/components/List.js
+++ frontend/my-app/src/components/List.js
@@ -2,6 +2,11 @@ import React from "react";
 import Item from "../components/Item";
 import { CircularProgress } from "@material-ui/core";
 import { createStateUtils } from "../lib/asyncUtils";
+import styled from "styled-components";
+
+const AlarmMsg = styled.div`
+  color: blue;
+`;
 
 const List = ({ list, loading, error }) => {
   if (loading) {
@@ -9,6 +14,9 @@ const List = ({ list, loading, error }) => {
   }
   if (error) {
     return <div>오류입니다... 잠시 후 다시시도해주세요.</div>;
+  }
+  if(list.length === 0){
+    return <AlarmMsg>할 일을 입력해보세요!</AlarmMsg>
   }
   return (
     <>
