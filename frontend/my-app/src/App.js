13dcc82be63fcc1a95becb8a213675296e37ba0c
gunkim0318 <gunkim0318@gmail.com>
2020-12-13 20:38:45 +0900

    Fragment태그 삭제

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import TodoList from "./pages/TodoList";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Fragment>
+    <>
       <Route path="/" exact component={Login} />
       <Route path="/list" component={TodoList} />
-    </Fragment>
+    </>
   );
 }
 
