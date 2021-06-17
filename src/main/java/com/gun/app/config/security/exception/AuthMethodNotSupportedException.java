1801a00329a6061c08b246665c8c80c6d28b5a85
gunkims <45007556+kanpann@users.noreply.github.com>
2021-06-18 00:49:40 +0900

    삭제된 파일 반영

--- src/main/java/com/gun/app/config/security/exception/AuthMethodNotSupportedException.java
+++
@@ -1,9 +0,0 @@
-package com.gun.app.config.security.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class AuthMethodNotSupportedException extends AuthenticationException {
-    public AuthMethodNotSupportedException(String msg) {
-        super(msg);
-    }
-}
