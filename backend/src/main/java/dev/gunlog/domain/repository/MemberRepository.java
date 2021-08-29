3714c25c96208de19d26f76fd67323eb5c7add86
gunkims <45007556+kanpann@users.noreply.github.com>
2021-08-29 23:41:48 +0900

    @Repository 어노테이션 추가

--- backend/src/main/java/dev/gunlog/domain/repository/MemberRepository.java
+++ backend/src/main/java/dev/gunlog/domain/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package dev.gunlog.domain.repository;
 
 import dev.gunlog.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByMemberId(String memberId);
 }
--- backend/src/main/java/dev/gunlog/domain/repository/TodoRepository.java
+++ backend/src/main/java/dev/gunlog/domain/repository/TodoRepository.java
@@ -3,10 +3,12 @@ package dev.gunlog.domain.repository;
 import dev.gunlog.domain.entity.Member;
 import dev.gunlog.domain.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findAllByMember(Member member);
     Optional<Todo> findByIdAndMember(long id, Member member);
